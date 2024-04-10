45641bade155b0ffe5a039878206918d148a50f6
Daniel Ribes <95689623+dannyribes@users.noreply.github.com>
2024-04-10 07:25:01 +0200

    Google Analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,13 +2,17 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import vercel from "@astrojs/vercel/serverless";
 import preact from "@astrojs/preact";
-
+import partytown from '@astrojs/partytown'
 import auth from "auth-astro";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), preact(), auth()],
-  buildOptions:{
+  integrations: [tailwind(), preact(), auth(), partytown({
+    config: {
+      forward: ["dataLayer.push"],
+    }
+  })],
+  buildOptions: {
     outDir: './dist',
   },
   i18n: {
